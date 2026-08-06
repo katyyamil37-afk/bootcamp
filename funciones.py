2faf4d6b038b68ccf389dd2b1e3bf561aa2a1443
katyyamil37-afk <katy.yamil37@gmail.com>
2026-08-06 11:38:36 -0400

    Create funciones.py

---
+++ funciones.py
@@ -0,0 +1,87 @@
+import csv
+import os
+
+AREAS = ('TI', 'Finanzas', 'RRHH', 'Comercial')
+
+def listar_empleados(empleados):
+    if not empleados:
+        print("No hay empleados registrados")
+        return
+
+    for empleado in empleados:
+        print(f"ID { empleado["id"] } | Nombre { empleado["nombre"] } | Edad { empleado["edad"] } | Área { empleado["area"] } | Tecnología { empleado["tecnologia"] }")
+
+def agregar_empleado(empleados, tecnologias):
+    identificador = int(input("Ingrese un identificador: "))
+    nombre = input("Ingrese el nombre: ")
+    edad = int(input("Ingrese la edad: "))
+
+    print("Elija el área: ")
+
+    for i, area in enumerate(AREAS, 1):
+        print(f"{i} {area}")
+
+    opcion = int(input("Seleccione un área: "))
+
+    tecnologia = input("Ingrese la tecnología: ")
+
+    empleado = {
+        "id": identificador,
+        "nombre": nombre,
+        "edad": edad,
+        "area": AREAS[opcion - 1],
+        "tecnologia": tecnologia
+    }
+
+    empleados.append(empleado)
+    tecnologias.add(tecnologia)
+
+def eliminar_empleado(empleados):
+    identificador = int(input("Ingrese el ID del empleado a eliminar: "))
+
+    for empleado in empleados:
+        if empleado["id"] == identificador:
+            empleados.remove(empleado)
+            print("Empleado ha sido eliminado")
+
+            return
+
+    print("No se encontró el empleado")
+
+def mostrar_resumen(empleados, tecnologias):
+    print(f"Total de empleados: { len(empleados) }")
+
+    print("Listado de tecnologías: ")
+    for tecnologia in tecnologias:
+        print(tecnologia)
+
+def guardar_csv(empleados):
+    with open("empleados.csv", "w", newline = "", encoding = "utf-8") as archivo:
+        campos = ["id", "nombre", "edad", "area", "tecnologia"]
+
+        escritor = csv.DictWriter(archivo, delimiter=";", fieldnames=campos)
+
+        escritor.writeheader()
+        escritor.writerows(empleados)
+
+    print("Se generó el archivo empleados.csv")
+
+def cargar_csv():
+    empleados = []
+    tecnologias = set()
+
+    if not os.path.exists("empleados.csv"):
+        return empleados, tecnologias
+
+    with open("empleados.csv", "r", newline = "", encoding = "utf-8") as archivo:
+        lector = csv.DictReader(archivo, delimiter=";")
+
+        for empleado in lector:
+            empleado["id"] = int(empleado["id"])
+            empleado["edad"] = int(empleado["edad"])
+
+            empleados.append(empleado)
+
+        print("Se han cargado los datos")
+
+    return empleados, tecnologias

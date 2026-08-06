5946f4cea50627eac8fd035ebd7ee85838c4c513
katyyamil37-afk <katy.yamil37@gmail.com>
2026-08-06 11:40:06 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+from funciones import listar_empleados, agregar_empleado, eliminar_empleado, mostrar_resumen, guardar_csv, cargar_csv
+                        
+
+def mostrar_menu():
+    print("\nSistema de Gestión de Datos de Empleados")
+    print("1. Agregar empleado")
+    print("2. Listar empleados")
+    print("3. Eliminar empleado")
+    print("4. Mostrar resumen")
+    print("5. Exportar datos")
+    print("0. Salir")
+
+def main():
+
+    empleados, tecnologias = cargar_csv()
+
+    while True:
+        mostrar_menu()
+
+        opcion = input("Ingrese una opción del menú [0..5]: ")
+
+        if opcion == "1":
+            print("Agregar empleado")
+            agregar_empleado(empleados, tecnologias)
+        elif opcion == "2":
+            print("Listar empleados")
+            
+            listar_empleados(empleados)
+        elif opcion == "3":
+            print("Eliminar empleado")
+
+            eliminar_empleado(empleados)
+        elif opcion == "4":
+            print("Mostrar resumen")
+
+            mostrar_resumen(empleados, tecnologias)
+        elif opcion == "5":
+            print("Exportar datos")
+
+            guardar_csv(empleados)
+        elif opcion == "0":
+            break
+        else:
+            print("Opción no válida")
+
+    print("Sistema Gestión de Datos de Empleados")
+
+main()
